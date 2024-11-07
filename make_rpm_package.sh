e9b0c48f0ba10f75e0150de748be16a8479cbf3b
JustZhenya <s404.gordon@gmail.com>
2024-11-07 14:27:32 +0300

    test

--- make_rpm_package.sh
+++ make_rpm_package.sh
@@ -2,23 +2,29 @@
 
 # Create directory structure
 echo Create directory structure
-mkdir ~/rpmbuild
-mkdir ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
-mkdir ~/rpmbuild/BUILDROOT/satdump-1.2.2$BUILD_NO-1.x86_64/
+cd ~
+rpmdev-setuptree
+
+# Copying files
+echo Copy files
+make install DESTDIR=~/satdump-build/
 
 # Create package info
 cat <<EOF >> ~/rpmbuild/SPECS/satdump.spec
 %global __requires_exclude libhackrf.so.0|libportaudio.so.2|librtlsdr.so.0|libsdrplay_api.so.3
 
 Name:       satdump
-Version:    1.2.2$BUILD_NO
-Release:    1
+Version:    1.2.2
+Release:    $BUILD_NO
 Summary:    SatDump
 Recommends: libhackrf.so.0()(64bit), libportaudio.so.2()(64bit), librtlsdr.so.0()(64bit), libsdrplay_api.so.3()(64bit)
 License:    GPLv3+
 
 %description
 A generic satellite data processing software.
+
+%build
+mv ~/satdump-build/* .
 
 %post
 /sbin/ldconfig
@@ -36,11 +42,6 @@ A generic satellite data processing software.
 /usr/lib64/libsatdump_interface.so
 /usr/share/applications/satdump.desktop
 EOF
-
-# Copying files
-echo Copy files
-make install DESTDIR=~/rpmbuild/BUILDROOT/satdump-1.2.2$BUILD_NO-1.x86_64/
-rm -rf /root/rpmbuild/BUILDROOT/satdump-1.2.2$BUILD_NO-1.x86_64/usr/include/
 
 # Create package
 echo Create package
